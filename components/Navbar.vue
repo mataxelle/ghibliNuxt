59cd0fc0420bbb2dc11c82b9e60f12be947e8f64
axelle <kadance9720@gmail.com>
2021-10-30 14:16:36 +0200

    Add Navbar and Footer

---
+++ components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <v-app-bar color="deep-purple accent-4" flat dark>
+      <!--<v-app-bar-nav-icon></v-app-bar-nav-icon>-->
+
+      <v-toolbar-title>GhibliNuxt</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn text>Films</v-btn>
+      
+      <v-btn text>Contact</v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-autocomplete
+            v-model="value"
+            :items="items"
+            filled
+            clearable
+            prepend-icon="mdi-search"
+            label="Recherche"
+        >
+      </v-autocomplete>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Navbar"
+}
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <Navbar />
+    <v-main>
+      <Nuxt />
+    </v-main>
+    <Footer />
+  </v-app>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  components: {
+    Navbar,
+    Footer
+  },
+
+  data() {
+    return {};
+  },
+};
+</script>
